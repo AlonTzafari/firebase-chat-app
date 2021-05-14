6ce27e460f294d920519b056ec7a21f2aff29684
AlonTzafari <77238176+AlonTzafari@users.noreply.github.com>
2021-05-14 12:22:48 +0300

    toggle theme button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.scss';
 import Auth from './components/Auth';
 import DisplayContainer from './components/DisplayContainer'; 
 import firebase from './database';
-
+import {themeContext} from './context';
 
 import {useAuthState} from 'react-firebase-hooks/auth';
 import {useState} from 'react';
@@ -10,15 +10,26 @@ import {useState} from 'react';
 
 const auth = firebase.auth();
 
+
 function App() {
 
+  
   const [theme, setTheme] = useState('dark');
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light');  
+  }
   const [user] = useAuthState(auth);
 
   return (
-    <div className={`App ${theme}`}>
-      {user ? <DisplayContainer user={user} /> : <Auth />}
-    </div>
+    <themeContext.Provider value={{theme, toggleTheme}}>
+      <div className={`App ${theme}`}>
+        {
+          user ?
+          <DisplayContainer user={user} /> :
+          <Auth />
+        }
+      </div>
+    </themeContext.Provider>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,8 @@
 import firebase from '../database';
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import ChatItem from './ChatItem';
+import {themeContext} from '../context';
+import {useContext} from 'react';
 
 function Home({openChat}) {
 
@@ -8,9 +10,10 @@ function Home({openChat}) {
     const messageRef = firestore.collection('chatRooms');
     const query = messageRef.where('users', 'array-contains', firebase.auth().currentUser.uid).orderBy('lastActivityAt', 'desc');
     const [chatRooms, loading, err] = useCollectionData(query, {idField: 'id'});
-
+    const {toggleTheme} = useContext(themeContext)
     return (
         <div>
+            <button onClick={toggleTheme}>THEME</button>
             {loading ? <h2>Loading...</h2> : 
             err ? <h2>Error loading chat rooms</h2> : 
             chatRooms.map(chatRoom => <ChatItem key={chatRoom.id} openChat={()=> openChat(chatRoom)} chatDetails={chatRoom} />)}
